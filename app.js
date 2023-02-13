d3e4bebd29c3edd23de83cd8fb1f7ac071cf1d1e
NaghmanShafique <naghman.shafique@gmail.com>
2023-02-14 01:27:14 +0500

    Data Saved.

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.get('/',(req , res) => {
-    res.send("Get API Hit by Naghman.")
+    // res.send("Get API Hit by Naghman.")
+    console.log("Message for Post API");
+    res.json(  
+             { "Name": "Naghman Shafeeque",
+               "Email" : "naghman.shafique@gmail.com",
+             "Password" : "welcome123" });
 });
 
 app.post('/user' , (req , res) => {
-    res.send('POST API Hit by Naghman.')
+   res.send("POST API Hit by Naghman.")
 });
 
 app.listen(port , () => {
