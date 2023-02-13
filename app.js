8b70ac1013baf9bc75c61a4c2eefec18573f99f7
NaghmanShafique <naghman.shafique@gmail.com>
2023-02-13 19:07:18 +0500

    Post API Saved

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.get('/',(req , res) => {
-    res.send("API Hit by Naghman Oracle Laptop.")
-})
+    res.send("Get API Hit by Naghman.")
+});
+
+app.post('/api/user' , (req , req) => {
+    res.send('POST API Hit by Naghman.')
+});
 
 app.listen(port , () => {
     console.log('Server is running on Port' + port);
