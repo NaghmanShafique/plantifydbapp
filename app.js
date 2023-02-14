5b65e08f4565608850a7df2c6a4e1a14599ea179
NaghmanShafique <naghman.shafique@gmail.com>
2023-02-14 18:25:12 +0500

    URI Changes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-   // res.send("Get API Hit by Naghman.")
+  
    console.log("Message for Post API");
    res.json(
       {
